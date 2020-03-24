a7001619530d1b89657c266f0d0dd326f205af10
jiahui97 <62423151+jiahui97@users.noreply.github.com>
2020-03-24 22:47:03 +0800

    happy swagger with description

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Hapi = require('hapi')
+const Hapi = require('@hapi/hapi')
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
 const HapiSwagger = require('hapi-swagger');
@@ -7,58 +7,77 @@ const Joi = require('@hapi/joi');
 
 (async () => {
   const server = await new Hapi.Server({
-      host: 'localhost',
-      port: 3000,
+    host: 'localhost',
+    port: 3000,
   });
 
 
-  
-const swaggerOptions = {
-  info: {
-          title: 'Test API Documentation',
-          version: Pack.version,
-      },
+
+  const swaggerOptions = {
+    info: {
+      title: 'Test API Documentation',
+      description: 'This is a sample example of API documentation.',
+      version: Pack.version,
+    },
   };
 
-await server.register([
-  Inert,
-  Vision,
-  {
+  await server.register([
+    Inert,
+    Vision,
+    {
       plugin: HapiSwagger,
       options: swaggerOptions
+    }
+  ]);
+
+  // add GET method
+  server.route({
+    method: 'GET',
+    path: '/get',
+    handler: (request, h) => {
+
+      return `Hello ${request.params.name}!`;
+
+    },
+    options: {
+      tags: ['api'],
+      validate: {
+        headers: Joi.object({
+          cookie: Joi.string().required()
+        }),
+        options: {
+          allowUnknown: true
+        }
+      },
+      description: 'swagger UI uses tags to group the display operations'
+    } 
+  });
+
+  // add POST method
+  server.route({
+    method: 'POST',
+    path: '/post',
+    handler: function (request, h) {
+
+      return 'Blog post added';
+
+    }, options: {
+      tags: ['api'],
+      validate: {
+        payload: Joi.object({
+          post: Joi.string().min(1).max(140),
+          date: Joi.date().required()
+        })
+      }, description: 'tags to API operation can be handled differently by tools & libraries'
+    }
+  });
+
+  try {
+    await server.start();
+    console.log('Server running at:', server.info.uri);
+  } catch (err) {
+    console.log(err);
   }
-]);
 
-
-// add GET method
-server.route({  
-  method: 'GET',
-  path: '/',
-  handler: (request, h) => {
-    return 'Hello World!'
-  }
-});
-
-// add POST method
-server.route({
-  method : 'POST',
-  path : '/hello',
-  handler : function (request, reply) {
-
-    const payload = request.payload;
-      return `Welcome ${payload.username}!`;
-
-  },options:{
-    tags:['api']
-  }
-});
-
-try {
-  await server.start();
-  console.log('Server running at:', server.info.uri);
-} catch(err) {
-  console.log(err);
-}
-
-server.route(Routes);
+  server.route(Routes);
 })();
