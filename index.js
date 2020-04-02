43dcfae842ac18fcdb5717345abf5ea257d92870
jiahui97 <62423151+jiahui97@users.noreply.github.com>
2020-04-02 14:17:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,83 +1,66 @@
+'use strict'
+require('env2')('env.json')
 const Hapi = require('@hapi/hapi')
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const HapiSwagger = require('hapi-swagger');
-const Pack = require('./package');
-const Joi = require('@hapi/joi');
+const Pack = require('./package.json')
 
-(async () => {
-  const server = await new Hapi.Server({
-    host: 'localhost',
+const init = async () => {
+  const server = Hapi.server({
     port: 3000,
-  });
+    host: 'localhost',
+    debug: { request: ['error'] }
+  })
 
-
+  await server.register(require('hapi-auth-jwt2'))
+  server.auth.strategy('jwt', 'jwt', {
+    key: process.env.JWT_SECRET,
+    validate: require('./util/jwtValidate')
+  })
+  server.auth.default('jwt')
 
   const swaggerOptions = {
     info: {
-      title: 'Test API Documentation',
-      description: 'This is a sample example of API documentation.',
-      version: Pack.version,
-    },
-  };
-
-  await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: swaggerOptions
+      title: 'Todo API Documentation',
+      version: Pack.version
     }
-  ]);
-
-  // add GET method
-  server.route({
-    method: 'GET',
-    path: '/get',
-    handler: (request, h) => {
-
-      return `Hello ${request.params.name}!`;
-
-    },
-    options: {
-      tags: ['api'],
-      validate: {
-        headers: Joi.object({
-          cookie: Joi.string().required()
-        }),
-        options: {
-          allowUnknown: true
-        }
-      },
-      description: 'swagger UI uses tags to group the display operations'
-    } 
-  });
-
-  // add POST method
-  server.route({
-    method: 'POST',
-    path: '/post',
-    handler: function (request, h) {
-
-      return 'Blog post added';
-
-    }, options: {
-      tags: ['api'],
-      validate: {
-        payload: Joi.object({
-          post: Joi.string().min(1).max(140),
-          date: Joi.date().required()
-        })
-      }, description: 'tags to API operation can be handled differently by tools & libraries'
-    }
-  });
-
-  try {
-    await server.start();
-    console.log('Server running at:', server.info.uri);
-  } catch (err) {
-    console.log(err);
   }
+  await server.register([
+    {
+      plugin: require('@hapi/inert')
+    }, {
+      plugin: require('@hapi/vision')
+    }, {
+      plugin: require('hapi-pgsql'),
+      options: {
+        database_url: process.env.DATABASE_URL
+      }
+    }, {
+      plugin: require('hapi-router'),
+      options: {
+        routes: 'routes/*.js'
+      }
+    }, {
+      plugin: require('hapi-swagger'),
+      options: swaggerOptions
+    }, {
+      plugin: require('hapi-redis2'),
+      options: {
+        settings: 'redis://127.0.0.1:6379/2',
+        decorate: true
+      }
+    }
+  ])
 
-  server.route(Routes);
-})();
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+
+  server.events.on('log', (event, tags) => {
+    console.log(event)
+  });
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
